bfe9392bf025a801c6dccd34dea19ac0df48b08d
Sora Tonami <ms0503@outlook.com>
2026-04-28 15:43:49 +0900

    feature: implement file downloader

--- src/download/curseforge.rs
+++ src/download/curseforge.rs
@@ -14,7 +14,7 @@ pub async fn get_file(entry: Mod, dir: &Path) -> Result<(), Box<dyn Error>> {
     };
     let out_filename = {
         let mut path = dir.to_path_buf();
-        path.push(name);
+        path.push(&name);
         path
     };
     unimplemented!();
--- src/download/file.rs
+++ src/download/file.rs
@@ -1,6 +1,8 @@
 use crate::config::Mod;
 use std::error::Error;
 use std::path::Path;
+use tokio::fs::File;
+use tokio::io;
 
 pub async fn get_file(entry: Mod, dir: &Path) -> Result<(), Box<dyn Error>> {
     let Mod::File {
@@ -13,9 +15,12 @@ pub async fn get_file(entry: Mod, dir: &Path) -> Result<(), Box<dyn Error>> {
     };
     let out_filename = {
         let mut path = dir.to_path_buf();
-        path.push(name);
+        path.push(&name);
         path
     };
-    unimplemented!();
+    let data = reqwest::get(url).await?.bytes().await?;
+    let mut out_file = File::create(out_filename).await?;
+    io::copy(&mut data.as_ref(), &mut out_file).await?;
+    println!("[INFO] {}: Downloaded", name);
     Ok(())
 }
