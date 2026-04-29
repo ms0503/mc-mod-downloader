fd3fc267a359405a2cf852275b4534359975ee8f
Sora Tonami <ms0503@outlook.com>
2026-04-29 14:17:05 +0900

    feature: download/file: add warning that there is security risk

--- src/download/file.rs
+++ src/download/file.rs
@@ -11,6 +11,10 @@ pub async fn get_file(entry: Mod, dir: &Path) -> Result<(), Box<dyn Error>> {
     else {
         unreachable!()
     };
+    println!(
+        "[WARN] {}: File URL downloader may pose a security risk",
+        name
+    );
     let out_filename = {
         let mut path = dir.to_path_buf();
         path.push(name);
