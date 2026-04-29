5217fe87d5808ebbcb0e2b77cdc279127bc7d046
Sora Tonami <ms0503@outlook.com>
2026-04-29 13:42:39 +0900

    refactor: tidy up generated code

--- src/download/modrinth.rs
+++ src/download/modrinth.rs
@@ -9,25 +9,6 @@ use tokio::io;
 const MODRINTH_API_HOST: &str = "api.modrinth.com";
 const MODRINTH_DOWNLOAD_HOST: &str = "cdn.modrinth.com";
 
-fn validate_modrinth_api_url(version_id: &str) -> Result<Url, Box<dyn Error>> {
-    let url = Url::parse(&format!(
-        "https://api.modrinth.com/v2/version/{}",
-        version_id
-    ))?;
-    if url.scheme() != "https" || url.host_str() != Some(MODRINTH_API_HOST) {
-        return Err("Invalid Modrinth API URL".into());
-    }
-    Ok(url)
-}
-
-fn validate_modrinth_download_url(raw: &str) -> Result<Url, Box<dyn Error>> {
-    let url = Url::parse(raw)?;
-    if url.scheme() != "https" || url.host_str() != Some(MODRINTH_DOWNLOAD_HOST) {
-        return Err("Invalid Modrinth download URL".into());
-    }
-    Ok(url)
-}
-
 pub async fn get_file(entry: Mod, dir: &Path) -> Result<(), Box<dyn Error>> {
     let Mod::Modrinth {
         name, ..
@@ -60,13 +41,34 @@ pub async fn get_download_url(entry: &Mod) -> Result<String, Box<dyn Error>> {
     };
     let api_url = validate_modrinth_api_url(version_id)?;
     let version = reqwest::get(api_url)
-    .await?
-    .json::<GetVersionResponse>()
-    .await?;
+        .await?
+        .json::<GetVersionResponse>()
+        .await?;
     let file = version
         .files
         .iter()
         .find(|file| file.filename == *name)
         .unwrap();
     Ok(file.url.clone())
+}
+
+fn validate_modrinth_api_url(version_id: &str) -> Result<Url, Box<dyn Error>> {
+    let url = Url::parse(&format!(
+        "https://api.modrinth.com/v2/version/{}",
+        version_id
+    ))?;
+    if url.scheme() == "https" && url.host_str() == Some(MODRINTH_API_HOST) {
+        Ok(url)
+    } else {
+        Err("Invalid Modrinth API URL".into())
+    }
+}
+
+fn validate_modrinth_download_url(raw: &str) -> Result<Url, Box<dyn Error>> {
+    let url = Url::parse(raw)?;
+    if url.scheme() == "https" && url.host_str() == Some(MODRINTH_DOWNLOAD_HOST) {
+        Ok(url)
+    } else {
+        Err("Invalid Modrinth download URL".into())
+    }
 }
